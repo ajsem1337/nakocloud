1cba41d864bc5da985327e2ed962e814b9dfdb56
ajsem <ajsem3147@gmail.com>
2024-04-22 14:21:35 +0200

    ᗜˬᗜ

--- lista/index.php
+++ lista/index.php
@@ -1,28 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-	<meta charset="UTF-8" />
+<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/fira_code.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"
 		integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous" />
 	<link rel="stylesheet" href="style.css" />
-	<title>kitty-zone</title>
+    <title>Document</title>
 </head>
-
-<body onload=" podmien()">
-	<nav>
-		<a href="../"><i class="bi bi-house"></i>home</a>
-		<a onclick="podmien()"><i class="bi bi-arrow-clockwise"></i>refresh image</a>
-	</nav>
-
-	<div id="kitku">
-		<img src="./" id="kot" alt=" " />
-	</div>
-
-	<script src="./main.js"></script>
+<body>
+    <nav>
+			<a href="../"><i class="bi bi-house"></i> home</a>
+            <div>
+                <input type="search" name="" id="szukajka" placeholder="szukaj np. stereogatari"> 
+            </div>
+            <a href="admin.php"><i class="bi bi-file-earmark-lock"></i> admin</a>
+    </nav>
 </body>
-
 </html>
